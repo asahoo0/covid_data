692737be524d224015ad96498059a8fdccefaab8
asahoo-4 <107638080+asahoo-4@users.noreply.github.com>
2022-07-27 21:58:27 -0500

    Update viz2.js

--- viz2.js
+++ viz2.js
@@ -94,7 +94,7 @@ svg.selectAll("rect")
   .transition()
   .duration(800)
   .attr("y", function(d) { return y(d.Cases); })
-  .attr("height", function(d) { return height - y(d.Cases/100); })
+  .attr("height", function(d) { return height - y(d.Cases); })
   .delay(function(d,i){console.log(i) ; return(i*100)})
 
 })
